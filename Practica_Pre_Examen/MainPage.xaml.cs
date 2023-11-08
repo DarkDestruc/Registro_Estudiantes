0cb1fc846a5a6a3b83fa0561d137c33fb913a4e7
Moises Chavez <gchavez19956@gmail.com>
2023-11-07 21:12:21 -0500

    Cambio 2

--- Practica_Pre_Examen/MainPage.xaml.cs
+++ Practica_Pre_Examen/MainPage.xaml.cs
@@ -1,25 +1,51 @@
 ﻿namespace Practica_Pre_Examen
 {
     public partial class MainPage : ContentPage
+
     {
+        string titulo = "Registro";
         public Estudiante MiEstudiante { get; set; }
 
         public MainPage()
         {
             InitializeComponent();
             MiEstudiante = new Estudiante();
-
             BindingContext = MiEstudiante;
 
         }
 
-
-
-
-        private void ConfirmarBtn_Clicked(object sender, EventArgs e)
+            private void ConfirmarBtn_Clicked(object sender, EventArgs e)
         {
-            DisplayAlert("Registro", MiEstudiante.NombreEstud + " sus datos se ingresaron correctamente.","ok");
+            if (IsValid())
+            {
+                var mensaje = $"{MiEstudiante.NombreEstud} sus datos se ingresaron correctamente";
+             
+            }
+            
         }
+        private bool IsValid()
+        {
+            if (string.IsNullOrEmpty(MiEstudiante.NombreEstud)) 
+            { 
+            
+                DisplayAlert(titulo, "Debe Ingresar un Nombre", "Ok");
+                return false;
+            }
+            if (string.IsNullOrEmpty(MiEstudiante.ApellidoEstud))
+            {
+                DisplayAlert(titulo, "Debe Ingresar un Apellido", "Ok");
+                return false;
+            }
+            if (string.IsNullOrEmpty(MiEstudiante.MateriaEstud))
+            {
+                DisplayAlert(titulo, "Debe Ingresar una Materia", "Ok");
+                return false;
+            }
+            {
+
+            }
+            return true;
+                }
     }
     
 }
